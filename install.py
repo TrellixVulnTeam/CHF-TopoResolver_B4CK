0eb37278c9bce01cca581dcb24d3452ba0639a40
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-02 06:38:48 +0000

    Adding tarfile member sanitization to extractall()

--- install.py
+++ install.py
@@ -97,7 +97,26 @@ def prepare_redis():
 
     print('Extracting Redis...')
     with tarfile.open(redis_tgzfile, 'r:gz') as tgz_ref:
-        tgz_ref.extractall(tools_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner) 
+            
+        
+        safe_extract(tgz_ref, tools_dir)
 
     os.remove(redis_tgzfile)
 
@@ -151,7 +170,26 @@ def prepare_maven():
 
     print('Extracting Apache Maven...')
     with tarfile.open(maven_tgzfile, 'r:gz') as tgz_ref:
-        tgz_ref.extractall(tools_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner) 
+            
+        
+        safe_extract(tgz_ref, tools_dir)
 
     os.remove(maven_tgzfile)
 
